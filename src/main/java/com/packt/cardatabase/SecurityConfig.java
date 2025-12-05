5df8eddac7855045f914d7f58f473abaae15a099
Kanat-Koblandin <kobylandina@gmail.com>
2025-12-06 02:40:59 +0500

    Fix CORS for Netlify

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         
         // Порт 5173 (React)
-        config.setAllowedOrigins(Arrays.asList("*"));
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://stellar-zuccutto-26784a.netlify.app"));
         config.setAllowedMethods(Arrays.asList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(false);
